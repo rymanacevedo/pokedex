e8f1e956d696e7cae949f1232184add0378c1473
Ryan Acevedo <ryanacevedo@Ryans-Mac-mini.local>
2021-11-03 23:39:18 -0600

    added code for pagination

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
+interface Pagination {
+  itemsCount: number;
+  pageSize: number;
+}
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
-export class PaginationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class PaginationComponent {
+  public pagesArray: Array<number> = [];
+  public currentPage: number = 1;
+  @Input() set setPagination(pagination: Pagination) {
+    if (pagination) {
+      const pagesAmount = Math.ceil(
+        pagination.itemsCount / pagination.pageSize
+      );
+      this.pagesArray = new Array(pagesAmount).fill(1);
+    }
+  }
+  @Output() goToPage = new EventEmitter<number>();
+  public setPage(pageNumber: number): void {
+    if (pageNumber === this.currentPage)
+      return;
+    this.currentPage = pageNumber;
+    this.goToPage.emit(pageNumber);
   }
 
 }
